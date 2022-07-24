f1822e2bf2b04b3fb152d02692c653bdb8a2536d
fredkamm <105788241+fredkamm@users.noreply.github.com>
2022-07-24 14:14:57 -0400

    start button now disappears after clicking and the choice buttons disappear and the end page; next is to make the choice buttons proceed to the next question

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,31 +16,15 @@ var highscores = document.querySelector(".highscores");
 var questionTitle = document.querySelector(".title");
 var question = document.querySelector("#question");
 
-// New button elements
+// New button elements with id
 var choice1 = document.createElement("button");
+choice1.setAttribute("id", "choiceOne")
 var choice2 = document.createElement("button");
+choice2.setAttribute("id", "choiceTwo")
 var choice3 = document.createElement("button");
+choice3.setAttribute("id", "choiceThree")
 var choice4 = document.createElement("button");
-
-
-// global variables
-// var countDown= 60;
-
-// setting up the start button
-// // when start quiz button is clicked, question 1 appears with choices and timer will begin.
-// function choice1Btn(){
-//     questionTitle.textContent = "Question 1";
-//     question.textContent = "this is the first question";
-//     question.style.fontSize = "50px";
-//     choice1.textContent = "choice one";
-//     buttonTwo.textContent = "choice two";
-//     document.body[1].appendChild(buttonTwo);
-//     // choice2.style.visibility = "visible";
-//     // choice3.style.visibility = "visible";
-//     // choice4.style.visibility = "visible";
-// }
-
-// console.log(buttonTwo)
+choice4.setAttribute("id", "choiceFour")
 
 // Setting the countdown
 // used activity 09 to help get this right
@@ -62,7 +46,7 @@ document.getElementById('button1').addEventListener('click', function (){
   }, 1000);
 });
 
-// Function for the questions
+// Functions for the questions
 function questionOne(){
   questionTitle.textContent = "Question 1";
   question.textContent = "this is the first question";
@@ -81,31 +65,67 @@ function questionTwo() {
   questionTitle.textContent = "Question 2";
   question.textContent = "this is the first question";
   question.style.fontSize = "50px";
+  document.body.appendChild(choice1);
+  choice1.textContent = "Choice 1";
+  document.body.appendChild(choice2);
+  choice2.textContent = "Choice 2";
+  document.body.appendChild(choice3);
+  choice3.textContent = "Choice 3";
+  document.body.appendChild(choice4);
+  choice4.textContent = "Choice 4";
 }
 
 function questionThree() {
   questionTitle.textContent = "Question 3";
   question.textContent = "this is the first question";
   question.style.fontSize = "50px";
+  document.body.appendChild(choice1);
+  choice1.textContent = "Choice 1";
+  document.body.appendChild(choice2);
+  choice2.textContent = "Choice 2";
+  document.body.appendChild(choice3);
+  choice3.textContent = "Choice 3";
+  document.body.appendChild(choice4);
+  choice4.textContent = "Choice 4";
 }
 
 function questionFour() {
   questionTitle.textContent = "Question 4";
   question.textContent = "this is the first question";
   question.style.fontSize = "50px";
+  document.body.appendChild(choice1);
+  choice1.textContent = "Choice 1";
+  document.body.appendChild(choice2);
+  choice2.textContent = "Choice 2";
+  document.body.appendChild(choice3);
+  choice3.textContent = "Choice 3";
+  document.body.appendChild(choice4);
+  choice4.textContent = "Choice 4";
 }
 
 function questionFive() {
   questionTitle.textContent = "Question 5";
   question.textContent = "this is the first question";
   question.style.fontSize = "50px";
+  document.body.appendChild(choice1);
+  choice1.textContent = "Choice 1";
+  document.body.appendChild(choice2);
+  choice2.textContent = "Choice 2";
+  document.body.appendChild(choice3);
+  choice3.textContent = "Choice 3";
+  document.body.appendChild(choice4);
+  choice4.textContent = "Choice 4";
 }
 
 // function to create the game over screen;
 function gameOver() {
-    questionTitle.textContent = "Quiz is over!";
-    question.textContent = "time: 0";
+    questionTitle.textContent = "The Quiz is over!";
+    question.textContent = "Time: 0";
     question.style.fontSize = "21px";
+    choice1.style.display = "none";
+    choice2.style.display = "none";
+    choice3.style.display = "none";
+    choice4.style.display = "none";
   }
   
   setTimer();
